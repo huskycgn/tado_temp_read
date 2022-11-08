6fabe596ab89942110166724479313d5b071d8bf
joachimlehmann <joachim.lehmann@googlemail.com>
2022-11-08 01:42:16 +0100

    DB not working yet :/

---
+++ funcs.py
@@ -0,0 +1,37 @@
+import datetime
+import mariadb
+import requests
+
+from cred import *
+
+
+def get_tempdata():
+    parameters = {
+        'username': username,
+        'password': password
+    }
+
+    endpoint_home = f'https://my.tado.com/api/v2/homes/{homeid}/zones/1/state'
+
+    response = requests.get(url=endpoint_home, params=parameters)
+
+    json_data = (response.json())
+
+    temp = json_data['sensorDataPoints']['insideTemperature']['celsius']
+    humid = json_data['sensorDataPoints']['humidity']['percentage']
+    timestamp = datetime.datetime.now()
+    timestamp = format(timestamp, '%Y%m%dT%H%M')
+    output_dict = {'time': timestamp, 'temp': temp, 'humid': humid}
+    return output_dict
+
+
+connection = mariadb.connect(
+    host=db_host,
+    user=db_user,
+    password=db_pass,
+    database='temps')
+
+cursor = connection.cursor()
+# print(cursor.execute(operation='SELECT * FROM WZ;'))
+
+# print(get_tempdata())
--- main.py
+++ main.py
@@ -1,27 +1,5 @@
-import requests
-from cred import username, password, homeid
-import datetime
+from funcs import get_tempdata
 
 endpoint_general = 'https://my.tado.com/api/v2/me'
 
-parameters = {
-    'username': username,
-    'password': password
-}
-
-# response = requests.get(url=endpoint_general, params=parameters)
-
-
-endpoint_home = f'https://my.tado.com/api/v2/homes/{homeid}/zones/1/state'
-
-response = requests.get(url=endpoint_home, params=parameters)
-
-json_data = (response.json())
-
-temp = json_data['sensorDataPoints']['insideTemperature']['celsius']
-humid = json_data['sensorDataPoints']['humidity']['percentage']
-timestamp = datetime.datetime.now()
-
-print(timestamp)
-print(temp)
-print(humid)
+print(get_tempdata())
