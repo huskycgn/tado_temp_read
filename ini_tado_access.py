7caccbd0e3259744b60ce245d1e1f03e23dedc52
Joachim Lehmann <joachim.lehmann@googlemail.com>
2025-04-04 16:30:49 +0200

    printing stuff...

--- ini_tado_access.py
+++ ini_tado_access.py
@@ -8,6 +8,8 @@ req = requests.post(
     ),
 )
 
+print(req.json())
+
 device_id = input("Enter Device_ID: ")
 
 req2 = requests.post(
@@ -16,5 +18,7 @@ req2 = requests.post(
         client_id="1bb50063-6b0c-4d11-bd99-387f4a91cc46",
         device_code=device_id,
         grant_type="urn:ietf:params:oauth:grant-type:device_code",
-    )
+    ),
 )
+
+print(req2.json())
