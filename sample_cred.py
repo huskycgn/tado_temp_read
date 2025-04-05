f62c5cb63e81a49f0349e4ac18339b56ebdae081
Joachim Lehmann <joachim.lehmann@googlemail.com>
2025-04-05 14:05:33 +0200

    corrected sample_cred.py

--- sample_cred.py
+++ sample_cred.py
@@ -2,8 +2,8 @@
 username = "your.mail@example.com"
 password = "supersecretpassword"
 homeid = 00000  # the tado home-id
-db_host = "192.168.1.XX"  # Your mariadb instance
-db_port = 3306
+db_host = "192.168.1.XX"  # Your postgresql instance
+db_port = 5432
 db_name = "temps"  # your db name
 
 # dict with human-readable room name, db table and color for matplotlib.
@@ -17,7 +17,7 @@ ROOMS = {
 }
 db_user = "weatheruser"  # your mariadb user
 db_pass = "yourdbpass"
-HUE_USER = "kjoB0Pmnwze5LZx38rGkzS7uZ5gjSWIJrUgMmHQR"  # Hue API KEY
+HUE_USER = "yourHueuserID"  # Hue API KEY
 HUE_IP = "192.168.1.XX"  # IP of your HUE device.
 graph_folder = "/Volumes/home/temps/"  # Where you want the graph .pngs to be stored.
 WEATHER_API_KEY = "YourWeatherServiceAPI"
